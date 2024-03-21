1e0355a05b6fce4006527048ce8255bd68895ca6
Le Thanh Nhan <thanhnhan13082021@gmail.com>
2024-03-21 16:29:58 +0700

    init project

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,9 @@
+package helloworld;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello LE THANH NHAN");
+		System.out.println("Welcome to JAVA");
+		System.out.println("HELO AGAIN");
+	}
+}
