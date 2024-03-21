10af99be91ae4ade3b1af1f5ab57e9512f835d38
Le Thanh Nhan <thanhnhan13082021@gmail.com>
2024-03-21 19:39:54 +0700

    Scanner and sysout

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -1,9 +1,17 @@
 package helloworld;
 
+import java.util.Scanner;
+
 public class HelloWorld {
+
 	public static void main(String[] args) {
-		System.out.println("Hello LE THANH NHAN");
-		System.out.println("Welcome to JAVA");
-		System.out.println("HELO AGAIN");
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Your name is: ");
+		String name = scanner.nextLine();
+		System.out.println("Your age is: ");
+		int age = scanner.nextInt();
+		System.out.println("My name is " + name + ", age = " + age);
+		scanner.close();
 	}
 }
